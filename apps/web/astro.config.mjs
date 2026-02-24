55ab1f58a84f44e0a6a42d6f842d16fd4d7b7270
londonbrown <londonchaim@gmail.com>
2026-02-23 18:24:38 -0800

    change: open external links in new tab and stylize anchor tags to stand out from default text

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   site: "https://marginalia.londonchaim.com",
   trailingSlash: "always",
   integrations: [sitemap()],
   markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer" ]}]
+    ],
     shikiConfig: {
       theme: "github-light"
     }
